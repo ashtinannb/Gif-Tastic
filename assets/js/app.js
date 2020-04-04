4a0e99051a634f9a9307b7ca90ced7f173d7ba9a
Ashtin Brannon <ashtinannbrannon@gmail.com>
2020-04-03 19:07:47 -0500

    reformatted. Added shows array, & function for display buttons

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,10 +1,14 @@
-$(document).ready(function() {
-	var shows = [
-		'Game of Thrones',
-		'Reign',
-		'Scandal',
-		'Ozark',
-		'Frasier',
-		'Outlander'
-    ];
-    
+//My favorite tv shows array
+var shows = ["Reign", "Game of Thrones", "Outlander", "Frasier", "Sherlock", "The Walking Dead"]
+
+$("#buttons-view").on("click", ".shows", function (event) {
+    event.preventDefault();
+    var shows = $(this).attr("data-name");
+    displayShowgif(shows);
+});
+
+//display buttons
+function displayShowgif(shows) {
+	var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
+	shows + "&api_key=ib4EGjnyaQ4EBPp9kHMnJk21rnKCk9zr&limit=10";
+
